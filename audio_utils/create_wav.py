8e0d46cd17ecda2ccfc7f003fb9f2851cac71234
gaganchandra765 <gaganchandra765@gmail.com>
2025-11-10 22:00:22 +0530

    minor additions

--- audio_utils/create_wav.py
+++ audio_utils/create_wav.py
@@ -2,12 +2,12 @@
 # create_wav.py
 import numpy as np
 from scipy.io.wavfile import write
-
+# seting some sampling rate 
 sample_rate = 44100     
 duration_sec = 5.0      
 frequency_hz = 440       
 amplitude = 0.5          
-
+# this is the example audio file for testing 
 t = np.linspace(0.,duration_sec,int(sample_rate *duration_sec),endpoint=False)
 wave_data =amplitude*np.sin(2 *np.pi*frequency_hz *t)
 
